4155a4d90d74bb42063868ba350e51ba0420e8e8
VefxoNadiradze <olld00@bk.ru>
2024-10-08 17:14:01 +0400

    install react forms and add in the signin page

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,11 +1,32 @@
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
   return (
     <SignInParent>
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <input
+          {...register("email", { required: "This input is required" })}
+          type="email"
+          placeholder="Email"
+        />
+        {errors.email ? <p>{errors.email?.message}</p> : ""}
+        <input
+          {...register("password", { required: "This input is required" })}
+          type="password"
+          placeholder="Password"
+        />
+        {errors.password ? <p>{errors.password?.message}</p> : ""}
+
         <button>Sign in</button>
       </form>
     </SignInParent>
